64da03e76aa4943e9d8820cdda41591a71170f64
Arvin mostafaei <Arvinmostafaeii@gmail.com>
2022-03-14 13:39:08 +0330

    added pages to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Home from "./pages/Home"
+import Pages from "./pages/Pages";
 
 function App() {
   return (
     <div className="App">
       <h1>Hi resipe app</h1>
-      <Home />
+      <Pages />
     </div>
   );
 }
