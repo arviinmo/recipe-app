97a07e47f6847348ef603250e474a0879f16a616
Arvin mostafaei <Arvinmostafaeii@gmail.com>
2022-03-15 12:55:46 +0330

    add Splide and some style

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -20,26 +20,30 @@ function Popular() {
 
   return (
     <div>
-      {popular.map((recipe) => {
-        return (
-          <Wrapper>
-            <h3>Popular Picks</h3>
+      <Wrapper>
+        <h3>Popular Picks</h3>
 
-            <Splide>
-              {popular.map((recipe) => {
-                return (
-                  <SplideSlide>
-                    <Card>
-                      <p>{recipe.title}</p>
-                      <img src={recipe.image} alt="recipe.title" />
-                    </Card>
-                  </SplideSlide>
-                );
-              })}
-            </Splide>
-          </Wrapper>
-        );
-      })}
+        <Splide
+          options={{
+            perPage: 4,
+            arrows:false,
+            pagination:false,
+            drag: "free",
+            gap: "5rem",
+          }}
+        >
+          {popular.map((recipe) => {
+            return (
+              <SplideSlide>
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt="recipe.title" />
+                </Card>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapper>
     </div>
   );
 }
