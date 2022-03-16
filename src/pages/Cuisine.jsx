ea9171c53b73b1d92fd5f0c772a265e260fc53e1
Arvin mostafaei <Arvinmostafaeii@gmail.com>
2022-03-16 13:00:49 +0330

    make Cuisine work dynamically and console.log :id

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { Link, useParams } from "react-router-dom";
 
 function Cuisine() {
+  const [cuisine, setCuisine] = useState([]);
+  let params = useParams();
+
+  const getCuisine = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`
+    );
+    const recipes = await data.json();
+    setCuisine(recipes.results);
+  };
+
+  useEffect(() => {
+    getCuisine(params.id);
+    console.log(params.id)
+  }, [params.id]);
+
   return (
     <div>
-        <h4>Cuisine</h4>
+      <h4>Cuisine</h4>
     </div>
-  )
+  );
 }
 
-export default Cuisine
+export default Cuisine;
