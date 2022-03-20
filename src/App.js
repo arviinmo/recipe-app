4d390b0f959a3d5d7bdf1258eb27b9414de21da5
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-03-20 12:15:04 +0330

    added Search component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Catagory from "./components/Catagory";
 import Pages from "./pages/Pages";
 import { BrowserRouter } from "react-router-dom";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Search />
         <Catagory />
         <Pages />
       </BrowserRouter>
