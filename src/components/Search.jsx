6f19df2f4e9c40398ccf2d3b9f635335c7c4177b
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-03-20 13:29:13 +0330

    added useNavigate to Search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,19 @@
 import styled from "styled-components";
 import { useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import {useNavigate} from "react-router-dom"
 
 function Search() {
   const [input, setInput] = useState("");
+  const navigate = useNavigate()
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    navigate("/searched/"+input)
+  };
 
   return (
-    <FormStyle>
+    <FormStyle onSubmit={submitHandler}>
       <div>
         <FaSearch></FaSearch>
         <input
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Cuisine from "./Cuisine";
+import Searched from "./Searched";
 
 function Pages() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/cuisine/:id" element={<Cuisine />} />
+      <Route path="/searched/:search" element={<Searched />} />
     </Routes>
   );
 }
