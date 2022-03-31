41f769e34a9b7005261ad7c2591e0de0d12cc867
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-03-31 16:07:11 +0430

    send api request in Searched component

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, {useState, useEffect} from "react";
 
 function Searched() {
-  return (
-    <div>Searched</div>
-  )
+  const [searchedRecipes, setSearchedRacipes] = useState([]);
+
+  const getSearched = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${name}`
+    );
+    const recipes = await data.json();
+    setCuisine(recipes.results);
+  };
+
+  return <div>Searched</div>;
 }
 
-export default Searched
+export default Searched;
