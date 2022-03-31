6954a2c57e6532c9a563813349f4b580f96fb117
Arvin mostafaei <70845998+arviinmo@users.noreply.github.com>
2022-03-31 16:19:25 +0430

    added Racipe page

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Cuisine from "./Cuisine";
 import Searched from "./Searched";
+import Recipe from "./Recipe";
 
 function Pages() {
   return (
@@ -9,6 +10,7 @@ function Pages() {
       <Route path="/" element={<Home />} />
       <Route path="/cuisine/:id" element={<Cuisine />} />
       <Route path="/searched/:search" element={<Searched />} />
+      <Route path="/recipe/:name" element={<Recipe />} />
     </Routes>
   );
 }
